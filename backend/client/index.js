c01ffb3f25c3e04c1d50b5bb4fac46a41231121b
Harsh Maniar <hmaniar@nvidia.com>
2024-06-06 21:41:33 -0700

    Initial Boilerplate Commit

---
+++ backend/client/index.js
@@ -0,0 +1,15 @@
+
+export class JSONPlaceholderClient {
+    constructor() {
+        this.url = 'https://jsonplaceholder.typicode.com'
+    }
+
+    async fetch(method, pathname, data = {}) {
+        const url = this.url + pathname
+        const body = Object.keys(data).length > 0 ? data : undefined
+        const result = await fetch(url, { method, body }).then((response) => response.json())
+        return result
+    }
+}
+
+export const client = new JSONPlaceholderClient()
---
+++ backend/helpers/middleware/logger.js
@@ -0,0 +1,5 @@
+
+export function loggerMiddleware(req, res, next) {
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
