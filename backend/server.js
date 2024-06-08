b59581ff93eaa6ce7706efdbe7107ffe090e05cf
Harsh Maniar <hmaniar@nvidia.com>
2024-06-07 21:22:29 -0700

    Initial changes to configure backend app for TwitterX

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,49 @@
 import express from 'express'
+import mongoose from 'mongoose';
 import cors from 'cors'
 import { loggerMiddleware } from './helpers/middleware/logger.js'
-import { client } from './client/index.js'
+import User from './models/user.js';
+
+const PORT = 3001
+const MONGO_DB_URL = 'mongodb+srv://harshmaniar18:jIYZ2FvxB7asqSoM@twitterx.jtbulu3.mongodb.net/?retryWrites=true&w=majority&appName=TwitterX'
 
 const app = express()
-const PORT = 3001
-
+app.use(express.json());
 app.use(cors())
 app.use(loggerMiddleware)
 
-app.get('/todos', async (req, res) => {
-    // Proxy JSON Placeholder's API
-    const todos = await client.fetch('get', '/todos')
-    res.send(todos)
-})
+async function main() {
+    await mongoose.connect(MONGO_DB_URL).then(() => console.log('Database Connected!'))
 
-app.get('/', async (req, res) => {
-    res.send('Welcome to TwitterX Backend')
-})
+    // Add a new user
+    app.post('/addUser', async (req, res) => {
+        const { username, password } = req.body;
+        console.log(`Trying to add: ${username} - ${password}`)
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+        // Validate input
+        if (!username || !password) {
+            return res.status(400).send('Username and password are required');
+        }
+        try
+        {
+            const email = username
+            const newUser = new User({ username, password, email });
+            await newUser.save();
+            res.status(201).send('User created successfully');
+        }
+        catch (error)
+        {
+            console.error(error);
+            res.status(500).send('Error creating user');
+        }
+    });
+
+    app.get('/', async (req, res) => {
+        res.send('Welcome to TwitterX Backend!')
+    })
+
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+
+}
+
+main()
